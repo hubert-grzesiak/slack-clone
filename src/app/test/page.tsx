79636fda20750187f292c632be5faee35518d840
Hubert Grzesiak <vTenn3R@wp.pl>
2024-08-25 21:24:26 +0200

    initial

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="ml-5">
+      <ul className="list-disc list-inside">
+        <li>XDD</li>
+        <li>XDD</li>
+        <li>XDD</li>
+        <li>XDD</li>
+        <li>XDD</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
